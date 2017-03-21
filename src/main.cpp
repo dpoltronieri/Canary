5e1db5a056252557e8d3426e25868890faae1231
dpoltronieri <danppoltronieri@gmail.com>
2017-03-20 21:50:29 -0300

    Travis CI

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include "main.hpp"
 #include "dht.h"
 #include <ldr.hpp>
-#include <MQSensor.hpp>
+#include <../lib/MQ-Sensor/src/MQSensor.hpp>
+#include <../lib/PrintManager/src/PrintManager.hpp>
 
 #ifdef RH_PLATFORM_NANO
 # include <SoftwareSerial.h>
@@ -14,12 +15,22 @@ SoftwareSerial Serial1(10, 11); // RX, TX
 
 // Sensores
 dht DHTSensor;
-ldr LDRSensor = ldr(LDR_PIN);
+ldr LDRSensor          = ldr(LDR_PIN);
+MQSensor Dummy         = MQSensor::NewMQSensor(1, MQ_SENSOR_DUMMY);
+PrintManager BT_Serial = PrintManager(&Serial1); // Explicit ti pointer conversion
+
+/* MONTAGEM
+ * LDR/pot pino 4
+ * DHT pino 5
+ * VAZIO pino 1
+ * BLUETOOTH Serial1
+ *
+ * //*/
 
 // Variáveis de Ambiente
-uint16_t TemperatureHumidity[4];
+uint16_t TemperatureHumidity[10];
 
-
+// Build ffffffffffffffffffffffffffffsadsedfeasrfwerawewtaerg
 void setup(){
     Serial.begin(115200);
     // Por enquanto
@@ -29,9 +40,9 @@ void setup(){
     Serial.println();
     Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
 
-    #ifdef RH_PLATFORM_MEGA
+    // #ifdef RH_PLATFORM_MEGA
     Serial1.begin(115200);
-    #endif
+    // #endif
 }
 
 void loop(){
@@ -41,6 +52,12 @@ void loop(){
     TemperatureHumidity[2] = DHTSensor.humidity;
     TemperatureHumidity[3] = LDRSensor.check();
 
+    BT_Serial.addValue("th", DHTSensor.temperature, DHTSensor.humidity);
+
+    TemperatureHumidity[5] = Dummy.check();
+    BT_Serial.addValue("M", TemperatureHumidity[5]);
+
+    BT_Serial.sendData();
 
     delay(2000);
 } // loop
